18b7691fbf471ed388bfe321de438c00068639e9
jabaldoo <113414364+jabaldoo@users.noreply.github.com>
2025-07-12 23:05:51 +0200

    boom

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
 AOS.init();
+
+document.addEventListener("mousemove", function(e) {
+    const logo = document.querySelector('.logo');
+    const x = (window.innerWidth / 2 - e.clientX) / 20;
+    const y = (window.innerHeight / 2 - e.clientY) / 20;
+    logo.style.transform = `translateX(${x}px) translateY(${y}px)`;
+});
