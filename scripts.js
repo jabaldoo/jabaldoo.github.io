0510db8c4bee139f05a1cbc91cf2cf0185ed6c84
jabaldoo <michalmatera9@gmail.com>
2025-08-31 19:18:05 +0200

    added new section

--- scripts.js
+++ scripts.js
@@ -1,61 +1,61 @@
-AOS.init();
+document.addEventListener('DOMContentLoaded', function() {
+    AOS.init();
 
-document.addEventListener("mousemove", function(e) {
-    const logo = document.querySelector('.logo');
-    const x = (window.innerWidth / 2 - e.clientX) / 20;
-    const y = (window.innerHeight / 2 - e.clientY) / 20;
-    logo.style.transform = `translateX(${x}px) translateY(${y}px)`;
-});
-
-
-
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const popupMenu = document.querySelector('.popup-menu');
-
-hamburgerMenu.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle('open');
-    popupMenu.classList.toggle('open');
-});
-
-const popupLinks = document.querySelectorAll('.popup-menu a');
-
-popupLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        hamburgerMenu.classList.remove('open');
-        popupMenu.classList.remove('open');
+    document.addEventListener("mousemove", function(e) {
+        const logo = document.querySelector('.logo');
+        const x = (window.innerWidth / 2 - e.clientX) / 20;
+        const y = (window.innerHeight / 2 - e.clientY) / 20;
+        logo.style.transform = `translateX(${x}px) translateY(${y}px)`;
     });
-});
 
-const videoWrappers = document.querySelectorAll('.moje-prace-section .video-wrapper');
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const popupMenu = document.querySelector('.popup-menu');
 
-videoWrappers.forEach(wrapper => {
-    wrapper.addEventListener('click', () => {
-        if (window.innerWidth <= 768) {
-            wrapper.classList.add('video-active');
-            const iframe = wrapper.querySelector('iframe');
-            iframe.src += '&autoplay=1';
-        }
+    hamburgerMenu.addEventListener('click', () => {
+        hamburgerMenu.classList.toggle('open');
+        popupMenu.classList.toggle('open');
     });
-});
 
-const konsultacjaModal = document.getElementById('konsultacja-popup');
-const konsultacjaBtn = document.getElementById('konsultacja-btn');
-const konsultacjaCloseButton = konsultacjaModal.querySelector('.close-button');
+    const popupLinks = document.querySelectorAll('.popup-menu a');
 
-konsultacjaBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    konsultacjaModal.style.display = 'block';
-    setTimeout(() => konsultacjaModal.classList.add('open'), 10);
-});
+    popupLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            hamburgerMenu.classList.remove('open');
+            popupMenu.classList.remove('open');
+        });
+    });
 
-konsultacjaCloseButton.addEventListener('click', () => {
-    konsultacjaModal.classList.remove('open');
-    setTimeout(() => konsultacjaModal.style.display = 'none', 300);
-});
+    const videoWrappers = document.querySelectorAll('.moje-prace-section .video-wrapper');
 
-window.addEventListener('click', (e) => {
-    if (e.target == konsultacjaModal) {
+    videoWrappers.forEach(wrapper => {
+        wrapper.addEventListener('click', () => {
+            if (window.innerWidth <= 768) {
+                wrapper.classList.add('video-active');
+                const iframe = wrapper.querySelector('iframe');
+                iframe.src += '&autoplay=1';
+            }
+        });
+    });
+
+    const konsultacjaModal = document.getElementById('konsultacja-popup');
+    const konsultacjaBtn = document.getElementById('konsultacja-btn');
+    const konsultacjaCloseButton = konsultacjaModal.querySelector('.close-button');
+
+    konsultacjaBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        konsultacjaModal.style.display = 'block';
+        setTimeout(() => konsultacjaModal.classList.add('open'), 10);
+    });
+
+    konsultacjaCloseButton.addEventListener('click', () => {
         konsultacjaModal.classList.remove('open');
         setTimeout(() => konsultacjaModal.style.display = 'none', 300);
-    }
+    });
+
+    window.addEventListener('click', (e) => {
+        if (e.target == konsultacjaModal) {
+            konsultacjaModal.classList.remove('open');
+            setTimeout(() => konsultacjaModal.style.display = 'none', 300);
+        }
+    });
 });
